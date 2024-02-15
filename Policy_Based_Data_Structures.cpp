1920cfadc6875707b9ab4453e9366d53676ed15f
Lalit159 <96078523+Lalit159@users.noreply.github.com>
2024-02-16 01:09:25 +0530

    Update Policy_Based_Data_Structures.cpp

--- Policy_Based_Data_Structures.cpp
+++ Policy_Based_Data_Structures.cpp
@@ -4,8 +4,11 @@
 //Include the following header files in your code to use PBDS:
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less
+#include <iostream>
 //Namespace 
 using namespace __gnu_pbds;
+using namespace std;
 
 
 //Templates
@@ -20,12 +23,12 @@ less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
 
 
 int main() {
-    My_Set <int> a;
+    My_Multi_Set <int> a;
     a.insert(2);
-    a.insert(4);
+    a.insert(2);
     a.insert(3);
     a.insert(7);
     a.insert(5);
-    cout << *a.find_by_order(3) << endl; //output is 5
+    cout << *a.find_by_order(4) << endl; //output is 5
     cout << (a.order_of_key(10)) << endl; //output is 5
 }
